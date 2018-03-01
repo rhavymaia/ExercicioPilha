dc7f89c3db7c3e24dc9dd3fa84bcef3936393126
Rhavy Maia Guedes <rmg3@cin.ufpe.br>
2018-03-01 11:19:45 -0300

    Adicionei as funções de manipulação da pilha

--- Pilha.py
+++ Pilha.py
@@ -1,3 +1,18 @@
 class Pilha:
     def __init__(self):
         self.itens = []
+    
+    def isVazio(self):
+        return len(self.itens) == 0
+    
+    def length(self):
+        pass
+    
+    def push(self, item):
+        pass
+    
+    def peek(self):
+        return self.itens[-1]
+    
+    def pop(self):
+        pass
